02e5e001d5a8a4a10d049d49c951ae66036f41ab
noorslatif <noorslatif@gmail.com>
2026-06-17 21:43:56 -0500

    Initialize DataWarehouse database with schemas

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,18 @@
+USE master;
+GO
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END
+GO;
 
+CREATE DATABASE DataWarehouse;
+USE DataWarehouse;
+GO
+CREATE SCHEMA Bronze;
+GO
+CREATE SCHEMA Silver;
+GO
+CREATE SCHEMA Gold;
+GO
